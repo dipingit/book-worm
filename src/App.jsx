12153308fda0e3701d061aab31e08dff2d27b886
Dip Roy <diproyf87@gmail.com>
2025-04-20 19:48:23 -0300

    setting up project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <div>
+      <div className='container bg-red-500'>
+        <h1>Hello world!</h1>
+      </div>
+      
+    
+    </div>
+  )
+}
+
+export default App
