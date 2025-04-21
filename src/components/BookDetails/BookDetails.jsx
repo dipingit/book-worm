ad4e5283e9d76a77afd173b7a0b90275d9816864
Dip Roy <diproyf87@gmail.com>
2025-04-21 14:06:29 -0300

    added mark as read on book details page

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import { addToStoredList } from '../utility/addToDb';
 
 const BookDetails = () => {
     const booksData = useLoaderData();
@@ -8,13 +9,18 @@ const BookDetails = () => {
     const id = parseInt(params.bookId);
     const book = booksData.find(book => book.bookId === id)
     const {image, bookName} = book
+
+    const handleMarkAsRead = (id) => {
+
+        addToStoredList(id);
+    }
     return (
         <div className='m-12'>
             <h2>Book Name: {bookName}</h2>
             <img className='w-36 mr-4' src={image} />
             <br />
-            <button className='btn btn-outline mr-4 btn-accent'>Read</button>
-            <button className='btn btn-accent'>Wish List</button>
+            <button className='btn btn-outline mr-4 btn-accent' onClick={ () => handleMarkAsRead(id) }>Mark as Read</button>
+            <button className='btn btn-accent'>Add to Wish List</button>
         </div>
     );
    
---
+++ src/components/utility/addToDb.js
@@ -0,0 +1,30 @@
+
+const getStoredList = () => {
+
+    const storedListStr = localStorage.getItem('read-list');
+    if(storedListStr){
+
+        const storedList = JSON.parse(storedListStr);
+        return storedList;
+    }
+    else{
+        return [];
+    }
+
+}
+
+const addToStoredList = (id) => {
+
+    const storedList = getStoredList();
+    
+    if(storedList.includes(id)){
+        console.log(id, 'book already exists in the reading list');
+    }
+    else{
+        storedList.push(id);
+        const storedListStr = JSON.stringify(storedList);
+        localStorage.setItem('read-list', storedListStr);
+    }
+}
+
+export {addToStoredList};
